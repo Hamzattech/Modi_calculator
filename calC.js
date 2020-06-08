89c52657be8b4358245d05405b73e4f8cc2614af
Hamzattech <ridwanulllah@gmail.com>
2020-06-08 20:21:53 +0100

    newM

--- calC.js
+++ calC.js
@@ -1,20 +1,23 @@
-function input(number){
-    document.forms["form"].textInserted.value=document.forms["form"].textInserted.value+number;
-};
-function cancel(){
-    document.forms["form"].textInserted.value = null;
-};
-function equal(){
-    var exp = document.forms["form"].textInserted.value;
-    if(exp){
-        document.forms["form"].textInserted.value = eval(exp)
-    }
-};
-    function backward(){
-        var strng = document.forms["form"].textInserted.value;
-        document.forms["form"].textInserted.value = strng.substring(0, strng.length-1);
-     };
-     
+function input(number) {
+  document.forms["form"].textInserted.value =
+    document.forms["form"].textInserted.value + number;
+}
+function cancel() {
+  document.forms["form"].textInserted.value = null;
+}
+function equal() {
+  var exp = document.forms["form"].textInserted.value;
+  if (exp) {
+    document.forms["form"].textInserted.value = eval(exp);
+  }
+}
+function backward() {
+  var strng = document.forms["form"].textInserted.value;
+  document.forms["form"].textInserted.value = strng.substring(
+    0,
+    strng.length - 1
+  );
+}    
     //  var leftOffset = 100;
     //  var moveIt = function(){
         //  $("#calculator_heading").offset({left:leftOffset});
